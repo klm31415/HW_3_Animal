fe5a22468176f66cf0624cb367a1ce16c303d5f1
klm31415 <klm31415@gmail.com>
2023-09-18 22:47:02 +0300

    to do homework 3

--- src/Animal.java
+++ src/Animal.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Animal {
+public class Animal {
+    int distanceRun;
+    int distanceSwim;
+
+
+    public Animal(int distanceRun, int distanceSwim) {
+        this.distanceRun = distanceRun;
+        this.distanceSwim = distanceSwim;
+    }
+
+    public void run() {
+        System.out.println("пробежал " + this.distanceRun + " м.");
+        //this.distanceRun = distance;
+    }
+
+    public void swim() {
+        System.out.println("проплыл " + this.distanceSwim + " м.");
+        //this.distanceSwim = distance;
+    }
 }
--- src/Cat.java
+++ src/Cat.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Cat {
+public class Cat extends Animal {
+    int runLimit = 200;
+    int swimLimit = 0;
+
+    public Cat(int distanceRun, int distanceSwim) {
+        super(distanceRun, distanceSwim);
+    }
+
+    @Override
+    public void run() {
+        if (super.distanceRun <= this.runLimit) {
+            System.out.println("Кот пробежал " + this.distanceRun + " м.");
+        }
+    }
+
+    @Override
+    public void swim() {
+            System.out.println("Коты не плавают");
+    }
 }
